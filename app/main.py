8e718e15c2849c8a5bc7b04c2aa46ccc5babf080
Antonio <acuelop369@g.educaand.es>
2026-04-20 19:51:22 +0200

    fix: corregir nombre de importación de posiciones_formacion a posicion_formacion en el módulo principal

--- app/main.py
+++ app/main.py
@@ -22,10 +22,10 @@ from .models import (
     evento_partido,
     formacion,
     notificacion,
-    convocatoria,
+    convocatoria_partido,
     jornada,
-    posiciones_formacion,
-    alineacion,
+    posicion_formacion,
+    alineacion_partido,
     token_recuperacion,
     invitacion,
 )
